e51e239fc06ee40c279bdd580bd302131820c881
Thanh Tuyền <ltthahtyn945@gmail.com>
2025-03-14 17:20:49 +0700

    capnhap

---
+++ admin/quanTri/danhsachnguoidung.php
@@ -0,0 +1,55 @@
+<?php require('includes/header.php');
+  ?>
+<div>
+  <h3>
+    Trang danh sách người dùng
+  </h3>
+ 
+
+    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Danh Sách Người Dùng</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Mã Khách Hàng</th>
+                                            <th>Họ tên</th>
+                                            <th>Ngày sinh </th>
+                                            <th>Giới tính</th>
+                                            <th>Địa Chỉ</th>
+                                            <th>Số điện thoại</th>
+                                            <th>Mật khẩu</th>
+                                            <th>Email</th>
+                                            <th>Chức năng</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Mã Khách Hàng</th>
+                                            <th>Họ tên</th>
+                                            <th>Ngày sinh </th>
+                                            <th>Giới tính</th>
+                                            <th>Địa Chỉ</th>
+                                            <th>Số điện thoại</th>
+                                            <th>Mật khẩu</th>
+                                            <th>Email</th>
+                                            <th>Chức năng</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+
+          
+    </tbody>
+                                </table>
+</div>
+                        </div>
+                    </div>
+                </div>
+    
+         
+   <?php require('includes/footer.php'); ?>
+
---
+++ admin/quanTri/danhsachsanpham.php
@@ -0,0 +1,51 @@
+<?php require('includes/header.php'); ?>
+<div>
+  <h3>Trang danh sách sản phẩm</h3>
+
+  <!-- DataTales Example -->
+  <div class="card shadow mb-4">
+      <div class="card-header py-3">
+          <h6 class="m-0 font-weight-bold text-primary">Danh sách sản phẩm</h6>
+      </div>
+      <div class="card-body">
+          <div class="table-responsive">
+              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                      <tr>
+                          <th>Mã sản phẩm</th>
+                          <th>Tên sản phẩm</th>
+                          <th>Loại máy</th>
+                          <th>Tình trạng</th>
+                          <th>Giá</th>
+                          <th>Hình ảnh</th>
+                          <th>Danh mục</th>
+                          <th>Số lượng</th>
+                          <th>Đường kính</th>
+                          <th>Chức năng</th>
+                      </tr>
+                  </thead>
+                  <tfoot>
+                      <tr>
+                          <th>Mã sản phẩm</th>
+                          <th>Tên sản phẩm</th>
+                          <th>Loại máy</th>
+                          <th>Tình trạng</th>
+                          <th>Giá</th>
+                          <th>Hình ảnh</th>
+                          <th>Danh mục</th>
+                          <th>Số lượng</th>
+                          <th>Đường kính</th>
+                          <th>Chức năng</th>
+                      </tr>
+                  </tfoot>
+                  <tbody>
+
+                 
+                  </tbody>
+              </table>
+          </div>
+      </div>
+  </div>
+</div>
+
+<?php require('includes/footer.php'); ?>
